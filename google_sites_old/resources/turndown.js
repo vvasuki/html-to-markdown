0538003aaae547b902a2da27bb3536b1b99ba55e
vvasuki <vishvas.vasuki@gmail.com>
2021-10-28 10:40:46 +0530

    CU

--- google_sites_old/resources/turndown.js
+++ google_sites_old/resources/turndown.js
@@ -78,7 +78,7 @@ rules.heading = {
 };
 
 rules.blockquote = {
-  filter: ['blockquote', '.versetext'],
+  filter: ['blockquote'],
 
   replacement: function (content) {
     content = content.replace(/^\n+|\n+$/g, '');
@@ -86,6 +86,23 @@ rules.blockquote = {
     return '\n\n' + content + '\n\n'
   }
 };
+
+rules.verseheading = {
+  filter: '.versetext',
+
+  replacement: function (content) {
+    let match = content.match("(.+?॥) *([०-९]+)");
+    if (match != undefined && match.length > 2) {
+      let id = match[2];
+      if (id.length == 1) id = "०" + id;
+      content = `##### ${id} ${match[1]}`;
+    } else {
+      content = `## मूलम्\n> ${content.replace("\n", "\n> ")}`;
+    }
+    return '\n\n' + content + '\n\n'
+  }
+};
+
 
 rules.list = {
   filter: ['ul', 'ol'],
@@ -369,33 +386,6 @@ function filterValue (rule, node, options) {
     throw new TypeError('`filter` needs to be a string, array, or function')
   }
 }
-
-/**
- * The collapseWhitespace function is adapted from collapse-whitespace
- * by Luc Thevenard.
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2014 Luc Thevenard <lucthevenard@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 
 /**
  * collapseWhitespace(options) removes extraneous whitespace from an the given element.
